d7579ab169a329922153632c88e1fc5b488dff84
Darshil-bhungaliya <darshil1312@gmail.com>
2024-07-11 12:59:10 +0530

    fixed price discount

--- src/controllers/order.contollers.js
+++ src/controllers/order.contollers.js
@@ -4,19 +4,20 @@ import { insertquery, selectquery, updatequery } from "../helper/func.helper.js"
 export const user_order = (req, res) => {
     try {
         const { totalproduct, totaldiscount } = req.body;
-        const total_discount = JSON.stringify(totaldiscount);
+
         const { email } = req.user[0];
-
-        const results = [];
+        let totalproductid = [];
+        let totalquantity = 0;
+        let totalprice = 0;
+        let totalrevenue = 0;
         let queryCount = 0;
-
         for (let i = 0; i < totalproduct.length; i++) {
             const product_id = totalproduct[i].product_id;
             let quantity = totalproduct[i].product_quantity;
             const product_discount = JSON.stringify(totalproduct[i].product_discount);
             const product_discount_type = JSON.parse(product_discount).type
             const perproduct_discount = JSON.parse(product_discount).discount
-           
+
             const obj = {
                 field: "*",
                 tablename: "product_listing",
@@ -51,44 +52,64 @@ export const user_order = (req, res) => {
                         if (err) throw err;
                         const user_id = result[0].id;
                         if (user_id) {
-                            if(product_discount_type == 0){
+                            totalquantity += +quantity
+                            if (product_discount_type == 0) {
                                 perqtyprice = perqtyprice - perproduct_discount;
-                            }else if(product_discount_type == 1){
-                                 perqtyprice = perqtyprice - (perproduct_discount/100) * perqtyprice
+                                if (totaldiscount?.type == 0) {
+                                    quantity * perqtyprice > totaldiscount.discount / totalproduct.length ? totalrevenue += quantity * perqtyprice - totaldiscount.discount / totalproduct.length : totalrevenue += 0;
+                                }
+                                if (!totaldiscount?.type || totaldiscount?.type == 1) {
+                                    totalrevenue += quantity * perqtyprice;
+                                }
+
+                            } else if (product_discount_type == 1) {
+                                perqtyprice = perqtyprice - (perproduct_discount / 100) * perqtyprice;
+                                if (totaldiscount?.type == 0) {
+                                    quantity * perqtyprice > totaldiscount.discount / totalproduct.length ? totalrevenue += quantity * perqtyprice - totaldiscount.discount / totalproduct.length : totalrevenue += 0;
+                                }
+                                if (!totaldiscount?.type || totaldiscount?.type == 1) {
+                                    totalrevenue += quantity * perqtyprice;
+                                }
                             }
-                            const insertobj = {
-                                tablename: `user_order`,
-                                tablefield: `(product_id,product_quantity,product_discount,total_discount,user_id,order_price)`,
-                                tablevalue: `('${product_id}','${quantity}','${product_discount}','${total_discount}','${user_id}','${quantity * perqtyprice}')`
-                            };
-                            const insertsql = insertquery(insertobj);
-                            con.query(insertsql, function (err, result) {
-                                if (result) {
-                                    results.push({
+                            if (totaldiscount?.type == 0) {
+                                quantity * perqtyprice > totaldiscount.discount / totalproduct.length ? totalprice += quantity * perqtyprice - totaldiscount.discount / totalproduct.length : totalprice += 0;
+                            } else if (totaldiscount?.type == 1 || !totaldiscount?.type) {
+
+                                totalprice += quantity * perqtyprice;
+                            }
+                            console.log(totalprice, "totalprice")
+                            console.log(totalrevenue, "totalrevenue")
+                            totalproductid.push(product_id);
+                            queryCount++;
+                            if (queryCount === totalproduct.length) {
+                                if (totaldiscount?.type == 1) {
+                                    totalprice = totalprice - (totaldiscount.discount / 100) * totalprice
+                                    product_discount_type ? totalrevenue = totalrevenue - (totaldiscount.discount / 100) * totalrevenue : null
+                                }
+                                const insertobj = {
+                                    tablename: `user_order`,
+                                    tablefield: `(product_id,product_quantity,user_id,total_price,total_revenue)`,
+                                    tablevalue: `('[${totalproductid}]','${totalquantity}','${user_id}','${totalprice}','${totalrevenue}')`
+                                };
+                                const insertsql = insertquery(insertobj);
+                                con.query(insertsql, function (err, result) {
+                                    if (err) throw err;
+                                    res.status(200).json({
                                         status: 1,
-                                        message: `Your order Successfully Placed and your order quantity placed is ${quantity}`
+                                        message: `Your order Successfully Placed`
                                     });
-                                } else {
-                                    results.push({
-                                        status: 0,
-                                        message: `something went wrong`
-                                    });
-                                }
-                                queryCount++;
-                                if (queryCount === totalproduct.length) {
-                                    res.status(200).json(results);
-                                }
-                            });
+                                });
+
+                            }
                         }
                     });
                 } else {
-                    results.push({
-                        status: 0,
-                        message: "Product is out of stock"
-                    });
                     queryCount++;
                     if (queryCount === totalproduct.length) {
-                        res.status(200).json(results);
+                        res.status(200).json({
+                            status: 0,
+                            message: "Product is out of stock"
+                        });
                     }
                 }
             });
@@ -116,7 +137,7 @@ export const totalcost = (req, res) => {
                 console.log(result[0].id);
                 const obj = {
                     tablename: "user_order",
-                    field: "SUM(product_quantity) as totalquantity,SUM(order_price) as totalcost",
+                    field: "count(id) as totalorder,SUM(total_revenue) as totalrevenue,SUM(total_price) as totalcost",
                     wherecondition: `user_id = '${result[0].id}'`
                 };
                 const sql = selectquery(obj);
